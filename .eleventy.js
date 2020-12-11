bdcf8fe25658ad95edf701e567201a48da63acfd
John Puddephatt <john@jdp.org.uk>
2020-12-11 14:00:37 +0000

    add netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,7 @@
-const pluginRss = require('@11ty/eleventy-plugin-rss')
-const pluginNavigation = require('@11ty/eleventy-navigation')
 const markdownIt = require('markdown-it')
 
-const filters = require('./utils/filters.js')
-const transforms = require('./utils/transforms.js')
-const shortcodes = require('./utils/shortcodes.js')
-const iconsprite = require('./utils/iconsprite.js')
 
 module.exports = function (config) {
-    // Plugins
-    config.addPlugin(pluginRss)
-    config.addPlugin(pluginNavigation)
-
-    // Filters
-    Object.keys(filters).forEach((filterName) => {
-        config.addFilter(filterName, filters[filterName])
-    })
-
-    // Transforms
-    Object.keys(transforms).forEach((transformName) => {
-        config.addTransform(transformName, transforms[transformName])
-    })
-
-    // Shortcodes
-    Object.keys(shortcodes).forEach((shortcodeName) => {
-        config.addShortcode(shortcodeName, shortcodes[shortcodeName])
-    })
-
-    // Icon Sprite
-    config.addNunjucksAsyncShortcode('iconsprite', iconsprite)
-
-    // Asset Watch Targets
-    config.addWatchTarget('./src/assets')
 
     // Markdown
     config.setLibrary(
@@ -44,15 +14,13 @@ module.exports = function (config) {
         })
     )
 
-    // Layouts
-    config.addLayoutAlias('base', 'base.njk')
-    config.addLayoutAlias('post', 'post.njk')
 
     // Pass-through files
-    config.addPassthroughCopy('src/robots.txt')
-    config.addPassthroughCopy('src/site.webmanifest')
     config.addPassthroughCopy('src/assets/images')
-    config.addPassthroughCopy('src/assets/fonts')
+    config.addPassthroughCopy('src/assets/styles')
+    config.addPassthroughCopy('src/admin')
+    config.addPassthroughCopy('src/uploads')
+
 
     // Deep-Merge
     config.setDataDeepMerge(true)
